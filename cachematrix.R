812291be483e7b79b953b92b92e8fc482bcfca10
haharris <heather@harrisideas.com>
2020-10-16 10:58:14 -0500

    Updated comments
    Updated comments to more specifically and accurately describe functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## These functions generate, cache, and return the cache of the inverse
-## of a matrix.
+## These functions calculate the inverse of a specified matrix, and
+## store that inverse in cache.  If the inverse has already been 
+## solved and is in the cache, the value from cache will be returned.
 
-## This function creates a "matrix" object and caches its inverse.
+## This function creates a "matrix" object and creates a cache for the
+## inverse of a specified matrix input.  In order to return the inverse
+## stored in the cache, the cacheSolve function needs to be run on the 
+## matrix that was run through makeCacheMatrix.
+## (example input: a <- makeCacheMarix(b) **where "b" is some already
+## defined/specified solveable matrix**)
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -19,8 +25,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## This function computes the inverse of the matrix returned by 
-## makeCacheMatrix (the previous function).  If the inverse has already
-## been solved, this function will return the inverse from cache.
+## makeCacheMatrix function.  If the inverse has already
+## been solved and stored in cache created by makeCacheMatrix 
+## this function will return the inverse from cache.
+## (example input: cacheSolve(a) **see above for "a"**; will return
+## the inverse of the matrix b, either newly calculated or from cache)
 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
